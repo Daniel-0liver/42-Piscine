c389225b7c689e861cfe956693bffbc4dcadab7a
Daniel Oliveira <dtoliverbr@gmail.com>
2021-09-09 22:23:06 +0100

    c00 ex03

---
+++ c00/ex03/ft_print_numbers.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/09 22:20:54 by dateixei          #+#    #+#             */
+/*   Updated: 2021/09/09 22:20:54 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char	c)
+{
+	write(1, &c, 1);
+}
+
+void    ft_print_alphabet(void)
+{
+    char    c;
+
+    c = '0';
+    while (c <= '9')
+    {
+        ft_putchar(c);
+        c++;
+    }
+}
+
+int main()
+{
+    ft_print_alphabet();
+    return (0);
+}
