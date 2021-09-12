fd30f4612499ad22350a9fa0967efb9f0de5578a
Daniel Oliveira <dtoliverbr@gmail.com>
2021-09-12 22:13:36 +0100

    Update c00 ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -19,32 +19,27 @@ void	ft_putchar(char	c)
 
 void	ft_putnbr(int	nb)
 {
-	int rest;
-
-	if (nb < 0)
+	if (nb == -2147483648)
 	{
-		nb = nb * -1;
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
 		ft_putchar('-');
-	}
-	rest = nb % 10;
-	if (nb > 99)
-	{
-		nb = nb / 10;
-		ft_putnbr(nb);
-		ft_putchar(rest + '0');
-	}
-	else if (nb > 9)
-	{
-		ft_putchar(nb / 10 + '0');
-		ft_putchar(rest + '0');
+		ft_putnbr(-nb);
 	}
 	else 
-		ft_putchar(nb + '0');
+	{
+		if (nb > 9)
+			ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
 }
 
 int main(void)
 {
-	int nb = -2147483647;
+	int nb = 9;
 
 	ft_putnbr(nb);
 	return (0);
