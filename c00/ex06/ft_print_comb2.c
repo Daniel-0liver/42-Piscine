4410258ec242100b7d683b24ae752dcb8301b858
Daniel Oliveira <dtoliverbr@gmail.com>
2021-09-12 18:09:20 +0100

    c00 ex06

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -17,4 +17,47 @@ void	ft_putchar(char	c)
 	write(1, &c, 1);
 }
 
+void	ft_print_until_9899(int	num1, int	num2)
+{
+	if (num1 == 98 && num2 == 99)
+	{
+		ft_putchar(num1 / 10 + '0');
+		ft_putchar(num1 % 10 + '0');
+		ft_putchar(' ');
+		ft_putchar(num2 / 10 + '0');
+		ft_putchar(num2 % 10 + '0');
+	}
+	else
+	{
+		ft_putchar(num1 / 10 + '0');
+		ft_putchar(num1 % 10 + '0');
+		ft_putchar(' ');
+		ft_putchar(num2 / 10 + '0');
+		ft_putchar(num2 % 10 + '0');
+		write(1, ", ", 2);
+	}
+}
+
 void	ft_print_comb2(void)
+{
+	int	num1;
+	int	num2;
+
+	num1 = 0;
+	while (num1 <= 99)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 99)
+		{
+			ft_print_until_9899(num1, num2);
+			num2++;
+		}
+		num1++;
+	}
+}
+
+int main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
