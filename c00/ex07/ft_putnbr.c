7414f50d16d4bdda09e7056ac81915d839044557
Daniel Oliveira <dtoliverbr@gmail.com>
2021-09-12 21:50:45 +0100

    c00 ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -17,7 +17,35 @@ void	ft_putchar(char	c)
 	write(1, &c, 1);
 }
 
-void    ft_print_combn(int  n)
+void	ft_putnbr(int	nb)
 {
-    
+	int rest;
+
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		ft_putchar('-');
+	}
+	rest = nb % 10;
+	if (nb > 99)
+	{
+		nb = nb / 10;
+		ft_putnbr(nb);
+		ft_putchar(rest + '0');
+	}
+	else if (nb > 9)
+	{
+		ft_putchar(nb / 10 + '0');
+		ft_putchar(rest + '0');
+	}
+	else 
+		ft_putchar(nb + '0');
+}
+
+int main(void)
+{
+	int nb = -2147483647;
+
+	ft_putnbr(nb);
+	return (0);
 }
