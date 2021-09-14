bd08b748e1bd0873493a78e8b5fa0092074a4645
Daniel Oliveira <dtoliverbr@gmail.com>
2021-09-15 00:57:23 +0100

    update ex08 c00

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -17,7 +17,36 @@ void	ft_putchar(char	c)
 	write(1, &c, 1);
 }
 
+void	ft_print_array(int	array[], int	n)
+{
+	int	index;
+
+	while (array[n - 1] <= 9)
+	{
+		index = 0;
+		while (index < n)
+		{
+			ft_putchar(array[index] + '0');
+			index++;
+		}
+		write(1, ", ", 2);
+		array[n - 1]++;
+	}
+}
+
 void    ft_print_combn(int  n)
 {
-    
+    int array[n];
+	int	index;
+
+	index = -1;
+	while (index++ < n)
+		array[index] = index;
+	index = 0;
+	ft_print_array(array, n);
+}
+
+int main(void)
+{
+	ft_print_combn(1);
 }
